0ec8dff3fff41844ff0e53f65855870f2ecb0d6b
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-07-14 11:08:27 -0300

    Finish Controller and ADD MySQL Dependency

---
+++ java-junior/src/main/java/com/test/javajunior/controller/TodoController.java
@@ -0,0 +1,38 @@
+package com.test.javajunior.controller;
+
+import com.test.javajunior.entity.Todo;
+import com.test.javajunior.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @PostMapping
+    List<Todo> create(Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+}
