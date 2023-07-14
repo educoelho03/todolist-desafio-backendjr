59d624201a69958bacb62bfa6b551e481948f751
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-07-14 11:00:32 -0300

    Finish Service

---
+++ java-junior/src/main/java/com/test/javajunior/service/TodoService.java
@@ -0,0 +1,41 @@
+package com.test.javajunior.service;
+
+import com.test.javajunior.entity.Todo;
+import com.test.javajunior.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("nome").ascending()); // Realiza a lista com ordenação
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
